e99fdc254b7c99dc2d4d0d57580b92158fecb177
ThataBispo <thauane05.bs@gmail.com>
2023-10-26 11:18:22 -0300

    conexoes

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -1,3 +1,4 @@
+import { Input } from "../../components/Input";
 import { PageHeader } from "../../components/PageHeader";
 import { TeacherItem } from "../../components/TeacherItem";
 import "./styles.css";
@@ -7,20 +8,9 @@ export function TeacherList() {
     <div id="page-teacher-list" className="container">
       <PageHeader title="Estes são os professores disponíveis.">
         <form action="" id="search-teachers">
-          <div className="input-block">
-            <label htmlFor="subject">Matéria</label>
-            <input type="text" id="subject" />
-          </div>
-
-          <div className="input-block">
-            <label htmlFor="week-day">Dia da semana</label>
-            <input type="text" id="week-day" />
-          </div>
-
-          <div className="input-block">
-            <label htmlFor="time">Hora</label>
-            <input type="text" id="time" />
-          </div>
+          <Input name="subject" label="Matéria" />
+          <Input name="week_day" label="Dia da semana" />
+          <Input type="time" name="time" label="Hora" />
         </form>
       </PageHeader>
 
